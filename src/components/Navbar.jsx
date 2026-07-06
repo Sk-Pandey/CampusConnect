fdc5337c514a642f364da770531a45dbf7ed7f53
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-07 04:59:33 +0530

    Navar bar nablinks active inactive feature added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,61 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <>
       <header className="flex items-center py-2 pl-2">
-        <Link id="logo" to="/">
-          <h1 className="text-white text-3xl font-serif">CampusConnect</h1>
-        </Link>
-        <nav className="flex-1 text-xl font-semibold font-mono ">
+        <h1 className="text-white text-3xl font-serif">
+          <Link id="logo" to="/">
+            CampusConnect
+          </Link>
+        </h1>
+
+        <nav className="flex-1 text-xl font-semibold font-mono">
           <ul className="list-none flex justify-around">
-            <NavLink to="/">
-              <li className="cursor-pointer hover:text-rose-200">Home</li>
-            </NavLink>
-            <NavLink to="/about">
-              <li className="cursor-pointer hover:text-rose-200">About</li>
-            </NavLink>
-            <NavLink to="/contact">
-              <li className="cursor-pointer hover:text-rose-200">Contact</li>
-            </NavLink>
-            <NavLink to="/login">
-              <li className="cursor-pointer hover:text-rose-200">Login</li>
-            </NavLink>
+            <li className="cursor-pointer hover:text-rose-200">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "text-rose-300" : "text-white"
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+
+            <li className="cursor-pointer hover:text-rose-200">
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  isActive ? "text-rose-300" : "text-white"
+                }
+              >
+                About
+              </NavLink>
+            </li>
+
+            <li className="cursor-pointer hover:text-rose-200">
+              <NavLink
+                to="/contact"
+                className={({ isActive }) =>
+                  isActive ? "text-rose-300" : "text-white"
+                }
+              >
+                Contact
+              </NavLink>
+            </li>
+
+            <li className="cursor-pointer hover:text-rose-200">
+              <NavLink
+                to="/login"
+                className={({ isActive }) =>
+                  isActive ? "text-rose-300" : "text-white"
+                }
+              >
+                Login
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </header>
