21bdb4573786e465a6b235b40e7ca36ffffe440b
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-07 05:04:27 +0530

    Fixed Sidebar navlink isactive style along with End to not get two navlink active

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,31 +2,54 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
+  const navLinkClass = ({ isActive }) =>
+    isActive ? "text-rose-300" : "text-white";
+
   return (
     <>
       <header className="p-2">
         <nav className="flex flex-col h-[100vh] justify-around text-xl font-semibold font-serif p-4 list-none">
-          <NavLink to="">
-            <li>Dashbaord</li>
-          </NavLink>
-          <NavLink to="courses">
-            <li>Courses</li>
-          </NavLink>
-          <NavLink to="assignments">
-            <li>Assignments</li>
-          </NavLink>
-          <NavLink to="students">
-            <li>Students</li>
-          </NavLink>
-          <NavLink to="analytics">
-            <li>Analytics</li>
-          </NavLink>
-          <NavLink to="profile">
-            <li>Profile</li>
-          </NavLink>
-          <NavLink to="settings">
-            <li>Settings</li>
-          </NavLink>
+          <li>
+            <NavLink to="" end className={navLinkClass}>
+              Dashboard
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="courses" className={navLinkClass}>
+              Courses
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="assignments" className={navLinkClass}>
+              Assignments
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="students" className={navLinkClass}>
+              Students
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="analytics" className={navLinkClass}>
+              Analytics
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="profile" className={navLinkClass}>
+              Profile
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="settings" className={navLinkClass}>
+              Settings
+            </NavLink>
+          </li>
         </nav>
       </header>
     </>
