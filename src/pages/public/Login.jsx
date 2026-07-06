f46b3852b1fb3090f3021e683abf528624878996
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-06 20:57:44 +0530

    context created

--- src/pages/public/Login.jsx
+++ src/pages/public/Login.jsx
@@ -1,4 +1,10 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const navigate = useNavigate();
+  const { setUser } = useContext(AuthContext);
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
       <div className="w-full max-w-md rounded-2xl bg-white p-8 shadow-lg">
@@ -7,11 +13,29 @@ const Login = () => {
         </h1>
 
         <div className="space-y-4">
-          <button className="w-full rounded-xl bg-blue-600 py-3 text-lg font-semibold text-white transition hover:bg-blue-700">
+          <button
+            onClick={() => {
+              setUser({
+                name: "Shashikant",
+                role: "student",
+              });
+              navigate("/dashboard");
+            }}
+            className="w-full rounded-xl bg-blue-600 py-3 text-lg font-semibold text-white transition hover:bg-blue-700"
+          >
             Login as Student
           </button>
 
-          <button className="w-full rounded-xl bg-green-600 py-3 text-lg font-semibold text-white transition hover:bg-green-700">
+          <button
+            onClick={() => {
+              setUser({
+                name: "Admin",
+                role: "admin",
+              });
+              navigate("/dashboard");
+            }}
+            className="w-full rounded-xl bg-green-600 py-3 text-lg font-semibold text-white transition hover:bg-green-700"
+          >
             Login as Admin
           </button>
         </div>
