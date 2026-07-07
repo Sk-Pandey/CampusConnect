b420adfeff4f12522b16e166ed8cc0175e1fdc70
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-07 09:42:09 +0530

    Logout feature added

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,15 +1,23 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-
+import { Link, useNavigate } from "react-router-dom";
 const Topbar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    setUser({ name: "", role: "" });
+    navigate("/login");
+  };
 
   return (
     <>
       <header className="flex items-center justify-between p-2">
-        <span id="logo">
-          <h1 className="text-white text-3xl font-serif">CampusConnect</h1>
-        </span>
+        <h1 className="text-white text-3xl font-serif">
+          <Link id="logo" to="/dashboard">
+            CampusConnect
+          </Link>
+        </h1>
+
         <div className="space-x-6">
           <button className="cursor-pointer" id="notifications">
             🔔
@@ -18,6 +26,12 @@ const Topbar = () => {
             Welcome, {user.name ? user.name : "User"}
           </span>
           <span className="bg-white rounded-full p-1">🙍‍♂️</span>
+          <button
+            onClick={handleLogout}
+            className="cursor-pointer font-semibold py-1 px-2 bg-rose-400 rounded-lg active:scale-95"
+          >
+            Logout
+          </button>
         </div>
       </header>
     </>
